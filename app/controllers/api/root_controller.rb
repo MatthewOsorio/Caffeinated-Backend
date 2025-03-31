3c486596ad558ca94b84ea403f5d93a40f562b52
MatthewOsorio <mattosoriox@gmail.com>
2025-03-31 14:44:17 -0700

    cleaned up some files

--- app/controllers/api/root_controller.rb
+++ app/controllers/api/root_controller.rb
@@ -2,19 +2,6 @@ class Api::RootController < ApiController
   self.extra_actions = { test: :get }
 
   def root
-    render json: {
-      api: {
-        message: "Welcome to the API.",
-        how_to_authenticate: <<~END.lines.map(&:strip).join(" ")
-          You can use this API with your normal login session. Otherwise, you can insert your API
-          key into a Bearer Authorization header, or into the URL parameters with the name
-          `api_key`.
-        END
-      }
-    }
-  end
-
-  def test
     render json: { message: "Welcome to the API" }
   end
 end
--- test/models/coffee_review_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CoffeeReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
